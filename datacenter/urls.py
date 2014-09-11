0ab6b7488c8d38f4efb7980aecf9fc8f6c087ac9
Luis Cruz <luismirandacruz@gmail.com>
2014-09-11 10:54:25 +0100

    added activity log rest api

--- datacenter/urls.py
+++ datacenter/urls.py
@@ -5,6 +5,8 @@ from datacenter.views import *
 urlpatterns = patterns('datacenter.views',
     url(r'^users/$', UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view()),
+    url(r'^activitylogs/$', ActivityLogList.as_view()),
+    url(r'^activitylogs/(?P<pk>[0-9]+)/$', ActivityLogDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- datacenter/views.py
+++ datacenter/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from datacenter.models import User
-from datacenter.serializers import UserSerializer
+from datacenter.models import User, ActivityLog
+from datacenter.serializers import UserSerializer, ActivityLogSerializer
 
 from rest_framework import mixins
 from rest_framework import generics
@@ -13,8 +13,15 @@ class UserList(generics.ListCreateAPIView):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
         
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class ActivityLogList(generics.ListCreateAPIView):
+    queryset = ActivityLog.objects.all()
+    serializer_class = ActivityLogSerializer
+    
+class ActivityLogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ActivityLog.objects.all()
+    serializer_class = ActivityLogSerializer
